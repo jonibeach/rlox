17a863e0680d77fdefdf76a1a9f0ac7306cbe261
jonibeach <joni.t.ranta@gmail.com>
2025-05-04 22:21:17 +0300

    lexer struct and multi-char lexing

---
+++ src/lexer.rs
@@ -0,0 +1,165 @@
+use std::fmt::Display;
+
+pub struct Lexer {
+    errors: Vec<Symbol<Error>>,
+    tokens: Vec<Symbol<Token>>,
+    prev_token: Option<Token>,
+}
+
+impl Lexer {
+    pub fn errors(&self) -> &[Symbol<Error>] {
+        &self.errors
+    }
+
+    pub fn tokens(&self) -> &[Symbol<Token>] {
+        &self.tokens
+    }
+
+    pub fn new() -> Self {
+        Self {
+            errors: Vec::new(),
+            tokens: Vec::new(),
+            prev_token: None,
+        }
+    }
+
+    pub fn lex(&mut self, lines: impl IntoIterator<Item = impl AsRef<str>>) {
+        for (line, line_contents) in lines.into_iter().enumerate() {
+            let mut add_token = |inner: Token| self.tokens.push(Symbol { line, inner });
+            let mut add_error = |c: char| {
+                self.errors.push(Symbol {
+                    line,
+                    inner: Error(c),
+                })
+            };
+
+            for c in line_contents.as_ref().chars() {
+                match (self.prev_token, c) {
+                    (Some(Token::Equal), '=') => {
+                        add_token(Token::EqualEqual);
+                        self.prev_token = None;
+                        continue;
+                    }
+                    (Some(prev), ..) => {
+                        add_token(prev);
+                        self.prev_token = None;
+                    }
+                    (None, ..) => {}
+                };
+
+                let new_prev_token = match c {
+                    '(' => Token::LeftParen,
+                    ')' => Token::RightParen,
+                    '{' => Token::LeftBrace,
+                    '}' => Token::RightBrace,
+                    '-' => Token::Minus,
+                    '+' => Token::Plus,
+                    '/' => Token::Slash,
+                    '*' => Token::Star,
+                    ',' => Token::Comma,
+                    '.' => Token::Dot,
+                    ';' => Token::Semicolon,
+                    '=' => Token::Equal,
+                    invalid => {
+                        add_error(invalid);
+                        continue;
+                    }
+                };
+
+                self.prev_token = Some(new_prev_token);
+            }
+
+            if let Some(prev) = self.prev_token {
+                add_token(prev)
+            }
+        }
+    }
+}
+
+pub struct Error(char);
+
+pub struct Symbol<T> {
+    line: usize,
+    inner: T,
+}
+
+impl Display for Symbol<Token> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "{} {} null",
+            self.inner.display(),
+            Into::<&str>::into(&self.inner)
+        ))
+    }
+}
+
+impl Display for Symbol<Error> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "[line {}] Error: Unexpected character: {}",
+            self.line + 1,
+            self.inner.0,
+        ))
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Token {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Minus,
+    Plus,
+    Slash,
+    Star,
+    Comma,
+    Dot,
+    Semicolon,
+    Equal,
+    EqualEqual,
+}
+
+impl Into<&'static str> for &Token {
+    fn into(self) -> &'static str {
+        match self {
+            Token::LeftParen => "(",
+            Token::RightParen => ")",
+            Token::LeftBrace => "{",
+            Token::RightBrace => "}",
+            Token::Minus => "-",
+            Token::Plus => "+",
+            Token::Slash => "/",
+            Token::Star => "*",
+            Token::Comma => ",",
+            Token::Dot => ".",
+            Token::Semicolon => ";",
+            Token::Equal => "=",
+            Token::EqualEqual => "==",
+        }
+    }
+}
+
+impl Token {
+    fn display(&self) -> &'static str {
+        match self {
+            Self::LeftParen => "LEFT_PAREN",
+            Self::RightParen => "RIGHT_PAREN",
+
+            Self::LeftBrace => "LEFT_BRACE",
+            Self::RightBrace => "RIGHT_BRACE",
+
+            Self::Minus => "MINUS",
+            Self::Plus => "PLUS",
+            Self::Slash => "SLASH",
+            Self::Star => "STAR",
+
+            Self::Comma => "COMMA",
+            Self::Dot => "DOT",
+            Self::Semicolon => "SEMICOLON",
+
+            Self::Equal => "EQUAL",
+            Self::EqualEqual => "EQUAL_EQUAL",
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,10 @@
 use std::env;
-use std::fmt::Display;
 use std::fs;
 use std::io::{self, Write};
 
-struct Symbol {
-    line: usize,
-    kind: SymbolKind,
-}
+use lexer::Lexer;
 
-impl Display for Symbol {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.kind {
-            SymbolKind::Token(t) => f.write_fmt(format_args!(
-                "{} {} null",
-                t.display(),
-                Into::<char>::into(&t)
-            )),
-            SymbolKind::Error(c) => f.write_fmt(format_args!(
-                "[line {}] Error: Unexpected character: {c}",
-                self.line + 1
-            )),
-        }
-    }
-}
-
-enum SymbolKind {
-    Token(Token),
-    Error(char),
-}
-impl SymbolKind {
-    fn is_err(&self) -> bool {
-        match self {
-            Self::Error(..) => true,
-            Self::Token(..) => false,
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-enum Token {
-    LeftParen,
-    RightParen,
-
-    LeftBrace,
-    RightBrace,
-
-    Minus,
-    Plus,
-    Slash,
-    Star,
-
-    Comma,
-    Dot,
-    Semicolon,
-}
-
-macro_rules! impl_conversions {
-    ($($i:path=$c:literal,)*) => {
-        impl From<char> for SymbolKind {
-            fn from(value: char) -> Self {
-                match value {
-                    $($c => Self::Token($i),)*
-                    invalid => Self::Error(invalid),
-                }
-            }
-        }
-
-        impl Into<char> for &Token {
-            fn into(self) -> char {
-                match self {
-                    $($i => $c,)*
-                }
-            }
-        }
-    };
-}
-
-impl_conversions! {
-    Token::LeftParen='(',
-    Token::RightParen=')',
-
-    Token::LeftBrace='{',
-    Token::RightBrace='}',
-
-    Token::Minus='-',
-    Token::Plus='+',
-    Token::Slash='/',
-    Token::Star='*',
-
-    Token::Comma=',',
-    Token::Dot='.',
-    Token::Semicolon=';',
-}
-
-impl Token {
-    fn display(&self) -> &'static str {
-        match self {
-            Self::LeftParen => "LEFT_PAREN",
-            Self::RightParen => "RIGHT_PAREN",
-
-            Self::LeftBrace => "LEFT_BRACE",
-            Self::RightBrace => "RIGHT_BRACE",
-
-            Self::Minus => "MINUS",
-            Self::Plus => "PLUS",
-            Self::Slash => "SLASH",
-            Self::Star => "STAR",
-
-            Self::Comma => "COMMA",
-            Self::Dot => "DOT",
-            Self::Semicolon => "SEMICOLON",
-        }
-    }
-}
-
+pub mod lexer;
 fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -131,30 +22,21 @@ fn main() -> Result<(), String> {
                 String::new()
             });
 
-            let mut symbols = Vec::new();
+            let mut lexer = Lexer::new();
 
-            for (line, line_contents) in file_contents.lines().into_iter().enumerate() {
-                for c in line_contents.chars() {
-                    symbols.push(Symbol {
-                        line,
-                        kind: c.into(),
-                    })
-                }
-            }
+            lexer.lex(file_contents.lines());
 
-            let errors: Vec<_> = symbols.iter().filter(|s| s.kind.is_err()).collect();
-
-            for err in &errors  {
+            for err in lexer.errors() {
                 eprintln!("{err}")
             }
 
-            for token in symbols.iter().filter(|s| !s.kind.is_err()) {
+            for token in lexer.tokens() {
                 println!("{token}")
             }
 
             println!("EOF  null");
 
-            if errors.len() > 0 {
+            if lexer.errors().len() > 0 {
                 std::process::exit(65)
             }
         }
