94236f85b289f9cff6d9396ebbffa8c02fc4d254
jonibeach <joni.t.ranta@gmail.com>
2025-05-04 21:15:39 +0300

    other tokens

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,91 @@ use std::env;
 use std::fs;
 use std::io::{self, Write};
 
+#[derive(Clone, Copy)]
 enum Token {
     LeftParen,
     RightParen,
 
     LeftBrace,
-    RightBrace
+    RightBrace,
+
+    Minus,
+    Plus,
+    Slash,
+    Star,
+
+    Comma,
+    Dot,
+    Semicolon,
+}
+
+macro_rules! impl_conversions {
+    ($($i:path=$c:literal,)*) => {
+        impl TryFrom<char> for Token {
+            type Error = String;
+            fn try_from(value: char) -> Result<Self, Self::Error> {
+                let token = match value {
+                    $($c => $i,)*
+                    invalid => return Err(format!("Invalid token '{invalid}'")),
+                };
+
+                Ok(token)
+            }
+        }
+
+        impl Into<char> for &Token {
+            fn into(self) -> char {
+                match self {
+                    $($i => $c,)*
+                }
+            }
+        }
+    };
+}
+
+impl_conversions!{
+    Token::LeftParen='(',
+    Token::RightParen=')',
+
+    Token::LeftBrace='{',
+    Token::RightBrace='}',
+
+    Token::Minus='-',
+    Token::Plus='+',
+    Token::Slash='/',
+    Token::Star='*',
+
+    Token::Comma=',',
+    Token::Dot='.',
+    Token::Semicolon=';',
 }
 
 impl Token {
-    fn as_char(&self) -> char {
-        match self {
-            Self::LeftParen => '(',
-            Self::RightParen => ')',
-
-            Self::LeftBrace => '{',
-            Self::RightBrace => '}'
-        }
-    }
     fn display(&self) -> &'static str {
         match self {
             Self::LeftParen => "LEFT_PAREN",
             Self::RightParen => "RIGHT_PAREN",
-            
+
             Self::LeftBrace => "LEFT_BRACE",
-            Self::RightBrace => "RIGHT_BRACE"
+            Self::RightBrace => "RIGHT_BRACE",
+
+            Self::Minus => "MINUS",
+            Self::Plus => "PLUS",
+            Self::Slash => "SLASH",
+            Self::Star => "STAR",
+
+            Self::Comma => "COMMA",
+            Self::Dot => "DOT",
+            Self::Semicolon => "SEMICOLON",
         }
     }
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
+        return Ok(())
     }
 
     let command = &args[1];
@@ -54,26 +105,19 @@ fn main() {
             let mut tokens = Vec::new();
 
             for c in file_contents.chars() {
-                let token = match c {
-                    '(' => Token::LeftParen,
-                    ')' => Token::RightParen,
-                    '{' => Token::LeftBrace,
-                    '}' => Token::RightBrace,
-                    _ => panic!("Unknown token '{c}'"),
-                };
-
-                tokens.push(token);
+                tokens.push(TryInto::<Token>::try_into(c)?);
             }
 
             for t in tokens {
-                println!("{} {} null", t.display(), t.as_char());
+                println!("{} {} null", t.display(), Into::<char>::into(&t));
             }
 
             println!("EOF  null");
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
         }
     }
+
+    Ok(())
 }
